4678775091c00896270e8ce09c84419b1000003c
Salvador Ruvalcaba <ruvsolis@gmail.com>
2021-08-07 15:41:59 -0500

    remake template with blade laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,98 @@
+@extends('layout') 
+
+@section('content') 
+
+<!-- Contact Us -->
+<section class="contact-us section-space">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12">
+                <!-- Contact Form -->
+                <div class="contact-form-area m-top-30">
+                    <h4>Get In Touch</h4>
+                    <form class="form" method="get" action="/">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-group">
+                                    <div class="icon"><i class="fa fa-user"></i></div>
+                                    <input type="text" name="first_name" placeholder="First Name">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-group">
+                                    <div class="icon"><i class="fa fa-user"></i></div>
+                                    <input type="text" name="last_name" placeholder="Last Name">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-group">
+                                    <div class="icon"><i class="fa fa-envelope"></i></div>
+                                    <input type="email" name="email" placeholder="Type Subjects">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-group">
+                                    <div class="icon"><i class="fa fa-tag"></i></div>
+                                    <input type="text" name="subject" placeholder="Type Subjects">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group textarea">
+                                    <div class="icon"><i class="fa fa-pencil"></i></div>
+                                    <textarea type="textarea" name="message" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group button">
+                                    <button type="submit" class="bizwheel-btn theme-2">Send Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!--/ End contact Form -->
+            </div>
+            <div class="col-lg-5 col-md-5 col-12">
+                <div class="contact-box-main m-top-30">
+                    <div class="contact-title">
+                        <h2>Contact with us</h2>
+                        <p>euismod eu augue. Etiam vel dui arcu. Cras varius mieros pharetra, id aliquam metus venenatis. Donec sollicit</p>
+                    </div>
+                    <!-- Single Contact -->
+                    <div class="single-contact-box">
+                        <div class="c-icon"><i class="fa fa-clock-o"></i></div>
+                        <div class="c-text">
+                            <h4>Horarios</h4>
+                            <p>Friday - Saturday<br>9:00 AM - 6:00 PM (Todos los días)</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Contact -->
+                    <!-- Single Contact -->
+                    <div class="single-contact-box">
+                        <div class="c-icon"><i class="fa fa-phone"></i></div>
+                        <div class="c-text">
+                            <h4>Call Us Now</h4>
+                            <p>Tel.: 33 1516 3857<br> Mob.: 33 1315 6353</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Contact -->
+                    <!-- Single Contact -->
+                    <div class="single-contact-box">
+                        <div class="c-icon"><i class="fa fa-envelope-o"></i></div>
+                        <div class="c-text">
+                            <h4>Email Us</h4>
+                            <p>contact@bizwheel.com<br>contacto@tuweb.com</p>
+                        </div>
+                    </div>
+                    <!--/ End Single Contact -->
+                    <div class="button">
+                        <a href="/" class="bizwheel-btn theme-1">Our Works<i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>	
+<!--/ End Contact Us -->
+
+@stop() 
---
+++ resources/views/home.blade.php
@@ -0,0 +1,370 @@
+@extends('layout') 
+
+@section('content') 
+    <!-- Hero Slider -->
+    <section class="hero-slider style1">
+        <div class="home-slider">
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text"> 
+                                <div class="hero-text"> 
+                                    <h4>We are always ready to help you</h4>
+                                    <h1>Our Creative Designer Waiting for Projects</h1>
+                                    <div class="p-text">
+                                        <p>Nunc tincidunt venenatis elit. Etiam venenatis quam vel maximus bibendum Pellentesque elementum dapibus diam tristique</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="/contact" class="bizwheel-btn theme-1 effect">Work with us</a>
+                                        <a href="/" class="bizwheel-btn theme-2 effect">View Our Portfolio</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text"> 
+                                <div class="hero-text"> 
+                                    <h4>Your time is so important for us</h4>
+                                    <h1>Build Your WorldClass Brand with Bizwheel</h1>
+                                    <div class="p-text">
+                                        <p>Nunc tincidunt venenatis elit. Etiam venenatis quam vel maximus bibendum Pellentesque elementum dapibus diam tristique</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="/" class="bizwheel-btn theme-1 effect">Read our blog</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+            
+            <!-- Single Slider -->
+            <div class="single-slider" style="background-image:url('https://via.placeholder.com/1700x800.png')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-7 col-md-8 col-12">
+                            <div class="welcome-text"> 
+                                <div class="hero-text"> 
+                                    <h4>Our experties are waiting for you</h4>
+                                    <h1>Best Way to Represent your Next Business </h1>
+                                    <div class="p-text">
+                                        <p>Nunc tincidunt venenatis elit. Etiam venenatis quam vel maximus bibendum Pellentesque elementum dapibus diam tristique</p>
+                                    </div>
+                                    <div class="button">
+                                        <a href="/" class="bizwheel-btn theme-2 effect">Our Leaders</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->
+			</div>
+		</section>
+		<!--/ End Hero Slider -->
+		
+		<!-- Features Area -->
+		<section class="features-area section-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Feature -->
+						<div class="single-feature">
+							<div class="icon-head"><i class="fa fa-podcast"></i></div>
+							<h4><a href="/">Creative Design</a></h4>
+							<p>Aenean aliquet rutrum enimn scelerisque. Nam dictumanpo, antequis laoreet ullamcorper, velitsd odio scelerisque tod</p>
+							<div class="button">
+								<a href="/" class="bizwheel-btn"><i class="fa fa-arrow-circle-o-right"></i>Learn More</a>
+							</div>
+						</div>
+						<!--/ End Single Feature -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Feature -->
+						<div class="single-feature">
+							<div class="icon-head"><i class="fa fa-podcast"></i></div>
+							<h4><a href="/">Quality Service</a></h4>
+							<p>Aenean aliquet rutrum enimn scelerisque. Nam dictumanpo, antequis laoreet ullamcorper, velitsd odio scelerisque tod</p>
+							<div class="button">
+								<a href="/" class="bizwheel-btn"><i class="fa fa-arrow-circle-o-right"></i>Learn More</a>
+							</div>
+						</div>
+						<!--/ End Single Feature -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Feature -->
+						<div class="single-feature">
+							<div class="icon-head"><i class="fa fa-podcast"></i></div>
+							<h4><a href="/">On-time Delivery</a></h4>
+							<p>Aenean aliquet rutrum enimn scelerisque. Nam dictumanpo, antequis laoreet ullamcorper, velitsd odio scelerisque tod</p>
+							<div class="button">
+								<a href="/" class="bizwheel-btn"><i class="fa fa-arrow-circle-o-right"></i>Learn More</a>
+							</div>
+						</div>
+						<!--/ End Single Feature -->
+					</div>
+					<div class="col-lg-3 col-md-6 col-12">
+						<!-- Single Feature -->
+						<div class="single-feature">
+							<div class="icon-head"><i class="fa fa-podcast"></i></div>
+							<h4><a href="/">24/7 Live support</a></h4>
+							<p>Aenean aliquet rutrum enimn scelerisque. Nam dictumanpo, antequis laoreet ullamcorper, velitsd odio scelerisque tod</p>
+							<div class="button">
+								<a href="/" class="bizwheel-btn"><i class="fa fa-arrow-circle-o-right"></i>Learn More</a>
+							</div>
+						</div>
+						<!--/ End Single Feature -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Features Area -->
+		
+		<!-- Call To Action -->
+		<section class="call-action overlay" style="background-image:url('https://via.placeholder.com/1500x300')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 col-12">
+						<div class="call-inner">
+							<h2>Brand Products &amp; Creativity is our Fashion</h2>
+							<p>ehicula maximus velit. Morbi non tincidunt purus, a hendrerit nisi. Vivamus elementum</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-12">
+						<div class="button">
+							<a href="/" class="bizwheel-btn">Our Portfolio</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Call to action -->
+		
+		<!-- Services -->
+		<section class="services section-bg section-space">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="section-title style2 text-center">
+							<div class="section-top">
+								<h1><span>Creative</span><b>Service We Provide</b></h1><h4>We provide quality service &amp; support..</h4>
+							</div>
+							<div class="section-bottom">
+								<div class="text-style-two">
+									<p>Aliquam Sodales Justo Sit Amet Urna Auctor Scelerisquinterdum Leo Anet Tempus Enim Esent Egetis Hendrerit Vel Nibh Vitae Ornar Sem Velit Aliquam</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-12">
+						<!-- Single Service -->
+						<div class="single-service">
+							<div class="service-head">
+								<img src="https://via.placeholder.com/555x410" alt="#">
+								<div class="icon-bg"><i class="fa fa-handshake-o"></i></div>
+							</div>
+							<div class="service-content">
+								<h4><a href="/">Business Strategy</a></h4>
+								<p>Cras venenatis, purus sit amet tempus mattis, justo nisi facilisis metus, in tempus ipsum ipsum eu ipsum. Class aptent taciti</p>
+								<a class="btn" href="/"><i class="fa fa-arrow-circle-o-right"></i>View Service</a>
+							</div>
+						</div>
+						<!--/ End Single Service -->
+					</div>
+					<div class="col-lg-4 col-md-4 col-12">
+						<!-- Single Service -->
+						<div class="single-service">
+							<div class="service-head">
+								<img src="https://via.placeholder.com/555x410" alt="#">
+								<div class="icon-bg"><i class="fa fa-html5"></i></div>
+							</div>
+							<div class="service-content">
+								<h4><a href="/">Web Development</a></h4>
+								<p>Cras venenatis, purus sit amet tempus mattis, justo nisi facilisis metus, in tempus ipsum ipsum eu ipsum. Class aptent taciti</p>
+								<a class="btn" href="/"><i class="fa fa-arrow-circle-o-right"></i>View Service</a>
+							</div>
+						</div>
+						<!--/ End Single Service -->
+					</div>
+					<div class="col-lg-4 col-md-4 col-12">
+						<!-- Single Service -->
+						<div class="single-service">
+							<div class="service-head">
+								<img src="https://via.placeholder.com/555x410" alt="#">
+								<div class="icon-bg"><i class="fa fa-cube"></i></div>
+							</div>
+							<div class="service-content">
+								<h4><a href="/">Market Research</a></h4>
+								<p>Cras venenatis, purus sit amet tempus mattis, justo nisi facilisis metus, in tempus ipsum ipsum eu ipsum. Class aptent taciti</p>
+								<a class="btn" href="/"><i class="fa fa-arrow-circle-o-right"></i>View Service</a>
+							</div>
+						</div>
+						<!--/ End Single Service -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Services -->
+		
+		<!-- Testimonials -->
+		<section class="testimonials section-space" style="background-image:url('https://via.placeholder.com/1500x700')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-9 col-12">
+						<div class="section-title default text-left">
+							<div class="section-top">
+								<h1><b>Our Satisfied Clients</b></h1>
+							</div>
+							<div class="section-bottom">
+								<div class="text"><p>some of our great clients and their review</p></div>
+							</div>
+						</div>
+						<div class="testimonial-inner">
+							<div class="testimonial-slider">
+								<!-- Single Testimonial -->
+								<div class="single-slider">
+									<ul class="star-list">
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+									</ul>
+									<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <strong>industry's standard</strong> dummy text ever sinsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+									<!-- Client Info -->
+									<div class="t-info">
+										<div class="t-left">
+											<div class="client-head"><img src="https://via.placeholder.com/70x70" alt="#"></div>
+											<h2>Julias Dien <span>CEO / Creative IT</span></h2>
+										</div>
+										<div class="t-right">
+											<div class="quote"><i class="fa fa-quote-right"></i></div>
+										</div>
+									</div>
+								</div>
+								<!-- / End Single Testimonial -->
+								<!-- Single Testimonial -->
+								<div class="single-slider">
+									<ul class="star-list">
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+									</ul>
+									<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <strong>industry's standard</strong> dummy text ever sinsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+									<!-- Client Info -->
+									<div class="t-info">
+										<div class="t-left">
+											<div class="client-head"><img src="https://via.placeholder.com/70x70" alt="#"></div>
+											<h2>Buman Panama <span>Founder, Jolace Group</span></h2>
+										</div>
+										<div class="t-right">
+											<div class="quote"><i class="fa fa-quote-right"></i></div>
+										</div>
+									</div>
+								</div>
+								<!-- / End Single Testimonial -->
+								<!-- Single Testimonial -->
+								<div class="single-slider">
+									<ul class="star-list">
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+										<li><i class="fa fa-star"></i></li>
+									</ul>
+									<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <strong>industry's standard</strong> dummy text ever sinsimply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>	
+									<!-- Client Info -->
+									<div class="t-info">
+										<div class="t-left">
+											<div class="client-head"><img src="https://via.placeholder.com/70x70" alt="#"></div>
+											<h2>Donald Bonam <span>Developer, Soft IT</span></h2>
+										</div>
+										<div class="t-right">
+											<div class="quote"><i class="fa fa-quote-right"></i></div>
+										</div>
+									</div>
+								</div>
+								<!-- / End Single Testimonial -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Testimonials -->
+		
+		
+		<!-- Client Area -->
+		<div class="clients section-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="partner-slider">
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-1.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-2.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-3.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-4.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-5.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+							<!-- Single client -->
+							<div class="single-slider">
+								<div class="single-client">
+									<a href="#" target="_blank"><img src="img/client/client-6.png" alt="#"></a>
+								</div>
+							</div>
+							<!--/ End Single client -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Client Area -->
+
+@stop() 
